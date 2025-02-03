b3668744e07ff36e35465e5b2a3cc8c773a9442f
lerningdif <yatnia@gmail.com>
2025-02-03 20:27:31 +0700

    Add files via upload

---
+++ AlbumsService.js
@@ -0,0 +1,80 @@
+const { nanoid } = require('nanoid')
+const InvariantError = require('../../exceptions/InvariantError');
+const NotFoundError = require('../../exceptions/NotFoundError');
+const { Pool } = require('pg');
+const { mapAlbumsDBToModel } = require('../../utils');
+
+class AlbumsService {
+  constructor() {
+    this._pool = new Pool();
+  }
+
+  async addAlbum({ name, year }) {
+    const id = nanoid(16);
+    const createdAt = new Date().toISOString();
+    const updatedAt = createdAt;
+
+    const query = {
+      text: 'INSERT INTO albums VALUES($1, $2, $3, $4,) RETURNING id',
+      values: [name, year, createdAt, updatedAt],
+    };
+
+     const result = await this._pool.query(query);
+
+    if (!result.rows[0].id) {
+      throw new InvariantError('Album gagal ditambahkan');
+    }
+ 
+    return result.rows[0].id;
+  }
+
+  async getAlbums() {
+     const result = await this._pool.query('SELECT * FROM albums');
+return result.rows.map(mapAlbumsDBToModel);
+  }
+
+
+
+  async getAlbumById(id) {
+    const query = {
+        text: `
+            SELECT 
+                albums.id, 
+                albums.name, 
+                albums.year,
+                songs.id AS song_id, 
+                songs.title AS song_title, 
+                songs.performer AS song_performer
+            FROM albums 
+            LEFT JOIN songs ON albums.id = songs."albumId" 
+
+            WHERE albums.id = $1
+        `,
+        values: [id],
+    };
+    const result = await this._pool.query(query);
+ 
+    if (!result.rows.length) {
+      throw new NotFoundError('Album tidak ditemukan');
+    }
+ 
+    return result.rows.map(mapAlbumsDBToModel)[0];
+  }
+  
+  async deleteAlbumById(id) {
+    const query = {
+      text: 'DELETE FROM albums WHERE id = $1 RETURNING id',
+      values: [id],
+    };
+
+    const result = await this._pool.query(query);
+
+    if (!result.rows.length) {
+      throw new NotFoundError('Album gagal dihapus. Id tidak ditemukan');
+    }
+  }
+}
+
+
+
+module.exports = AlbumsService;
