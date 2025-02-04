c00dcc17509a226883172a8070e3cd1b558699c4
lerningdif <yatnia@gmail.com>
2025-02-04 20:40:18 +0700

    Update index.js

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,14 +1,14 @@
-const mapAlbumsDBToModel = ({
+const mapAlbumsToDBModel = ({
     name, year, created_at: createdAt, updated_at: updatedAt,
   }) => ({
     name, year, createdAt, updatedAt,
   });
   
-  const mapSongsDBToModel = ({
+  const mapSongToDBModel = ({
     title, year, genre, performer, duration, albumId, created_at: createdAt,
     updated_at: updatedAt,
   }) => ({
     title, year, genre, performer, duration, albumId, createdAt, updatedAt,
   });
   
-  module.exports = { mapAlbumsDBToModel, mapSongsDBToModel };
+  module.exports = { mapAlbumsToDBModel, mapSongToDBModel };
