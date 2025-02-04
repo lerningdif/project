c8e23d5a0b05840232256192df3707954d6d3880
lerningdif <yatnia@gmail.com>
2025-02-04 16:19:41 +0700

    Update AlbumsService.js

--- src/services/postgres/AlbumsService.js
+++ src/services/postgres/AlbumsService.js
@@ -16,7 +16,7 @@ class AlbumsService {
 
     const query = {
       text: 'INSERT INTO albums VALUES($1, $2, $3) RETURNING id',
-      values: [id, name, year, ],
+      values: [id, name, year],
     };
 
      const result = await this._pool.query(query);
