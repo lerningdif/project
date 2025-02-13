a6634f4e9d75b1bd51fd3e51130f0e77e91456ac
lerningdif <yatnia@gmail.com>
2025-02-14 03:47:43 +0700

    Update index.js

--- src/api/playlist_songs/index.js
+++ src/api/playlist_songs/index.js
@@ -4,8 +4,8 @@ const routes = require("./routes");
 module.exports = {
   name: "playlist_songs",
   version: "1.0.0",
-  register: async (server, { service, validator }) => {
-    const playlist_songsHandler = new Playlist_songsHandler(service, validator);
+  register: async (server, { playlist_songsService, playlistsService, validator }) => {
+    const playlist_songsHandler = new Playlist_songsHandler(playlist_songsService, playlistsService, validator);
     server.route(routes(playlist_songsHandler));
   },
 };
