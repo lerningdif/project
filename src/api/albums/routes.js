cc79a20d1c08e06be6223f555f940d8b9566d12d
lerningdif <yatnia@gmail.com>
2025-02-19 15:52:53 +0700

    Update routes.js

--- src/api/albums/routes.js
+++ src/api/albums/routes.js
@@ -3,27 +3,27 @@ const routes = (handler) => [
     method: 'POST',
     path: '/albums',
     handler: handler.postAlbumHandler,
-  },
+},
   {
     method: 'GET',
     path: '/albums',
     handler: handler.getAlbumsHandler,
-  },
+},
   {
     method: 'GET',
     path: '/albums/{id}',
     handler: handler.getAlbumByIdHandler,
-  },
+},
   {
     method: 'PUT',
     path: '/albums/{id}',
     handler: handler.putAlbumByIdHandler,
-  },
+},
   {
     method: 'DELETE',
     path: '/albums/{id}',
     handler: handler.deleteAlbumByIdHandler,
-  },
+},
 ];
 
 module.exports = routes;
