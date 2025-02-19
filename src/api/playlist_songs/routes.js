378cbe6e3b6fda6e6e677095c438995229207e56
lerningdif <yatnia@gmail.com>
2025-02-19 15:57:27 +0700

    Update routes.js

--- src/api/playlist_songs/routes.js
+++ src/api/playlist_songs/routes.js
@@ -1,28 +1,28 @@
 const routes = (handler) => [
-    {
-      method: 'POST',
-      path: '/playlists/{playlistId}/songs',
-      handler: handler.postPlaylist_songHandler,
-      options: {
-      auth: 'openmusicapp_jwt',
-    },
-    },
-    {
-      method: 'GET',
-      path: '/playlists/{playlistId}/songs',
-      handler: handler.getPlaylist_songsHandler,
-  options: {
-      auth: 'openmusicapp_jwt',
-    },
-    },
-    {
-      method: 'DELETE',
-      path: '/playlists/{playlistId}/songs',
-      handler: handler.deletePlaylist_songByIdHandler,
-  options: {
-      auth: 'openmusicapp_jwt',
-    },
-    },
-  ];
- 
-  module.exports = routes;
+  {
+    method: 'POST',
+    path: '/playlists/{playlistId}/songs',
+    handler: handler.postPlaylist_songHandler,
+    options: {
+    auth: 'openmusicapp_jwt',
+  },
+  },
+  {
+    method: 'GET',
+    path: '/playlists/{playlistId}/songs',
+    handler: handler.getPlaylist_songsHandler,
+    options: {
+    auth: 'openmusicapp_jwt',
+  },
+  },
+  {
+    method: 'DELETE',
+    path: '/playlists/{playlistId}/songs',
+    handler: handler.deletePlaylist_songByIdHandler,
+options: {
+    auth: 'openmusicapp_jwt',
+  },
+  },
+];
+
+module.exports = routes;
